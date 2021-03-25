96a4c13fa6a23860ad03f57858ab10bb45d5e369
Brandon Kwon <bkwon23@amherst.edu>
2021-03-24 21:35:11 -0700

    Committing and Pushing

---
+++ Homework/scrape_justices.R
@@ -0,0 +1,11 @@
+url <- "https://en.wikipedia.org/wiki/List_of_justices_of_the_Supreme_Court_of_the_United_States"
+tables <- (
+  url %>%
+    read_html() %>%
+    html_nodes("table"))
+  html_table()
+  justice <- html_table(tables[[2]], fill = TRUE)
+  justice
+write_csv(justice, file = "justices.csv")
+
+
